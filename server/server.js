025ef0345fee9803def59c0aae92c6c0d87fbc9c
tawatchai.w <tawatchai.w@nextcorp.co.th>
2018-11-08 11:39:27 +0700

    price 0 cm

--- server/server.js
+++ server/server.js
@@ -393,8 +393,8 @@ const formatOrder = (data) => {
 สินค้า: ${data.product
             ? data.product.map((p, i) => '\n' + p.code + ':' + p.name + ' ' + p.amount + 'ชิ้น')
             : 'undefined'}
-${data.bank == 'ชำระปลายทาง: COD' && ['A', 'K'].indexOf(data.name.substr(0, 1)) == -1 ? 'undefined' : 'ธนาคาร: '+ data.bank} 
-ยอดชำระ: ${data.price ? formatMoney(data.price, 0) + ' บาท ' : 'undefined'}
+${data.bank == 'ชำระปลายทาง: COD' && ['A', 'K'].indexOf(data.name.substr(0, 1)) == -1 ? 'undefined' : 'ธนาคาร: ' + data.bank} 
+ยอดชำระ: ${data.price || data.bank == 'CM' ? formatMoney(data.price, 0) + ' บาท ' : 'undefined'}
 FB/Line: ${data.fb} 
 เพจ: ${data.page}`;
 }
