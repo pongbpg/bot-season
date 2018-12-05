6dadd5ce7b27776a165d2f061cf0fc178744f6e4
tawatchai.w <tawatchai.w@nextcorp.co.th>
2018-12-05 18:28:38 +0700

    add report cost

--- src/components/ReportPage.js
+++ src/components/ReportPage.js
@@ -289,6 +289,27 @@ export class ReportPage extends React.Component {
                                     </td>
                                 </tr>
                                 )}
+                                {['stock', 'owner'].indexOf(this.state.auth.role) > -1 && (< tr >
+                                    <td className="has-text-centered">9</td>
+                                    <td className="has-text-centered">กำไรขาดทุนแต่ละเพจ (วันที่เริ่ม-ถึงวันที่)</td>
+                                    <td className="has-text-centered">
+                                        <div className="field is-grouped is-grouped-centered">
+                                            <p className="control">
+                                                <a className="button is-danger is-centered is-small"
+                                                    href={`http://yaumjai.com:3000/api/report/dailyCost?uid=${this.state.uid}&startDate=${moment(this.state.startDate).format('YYYY-MM-DD')}&endDate=${moment(this.state.endDate).format('YYYY-MM-DD')}&file=pdf&sum=${this.state.sum}`}
+                                                    target="_blank">
+                                                    PDF</a>
+                                            </p>
+                                            <p className="control">
+                                                <a className="button is-success is-centered is-small"
+                                                    href={`http://yaumjai.com:3000/api/report/dailyCost?uid=${this.state.uid}&startDate=${moment(this.state.startDate).format('YYYY-MM-DD')}&endDate=${moment(this.state.endDate).format('YYYY-MM-DD')}&file=excel&sum=${this.state.sum}`}
+                                                    target="_blank">
+                                                    EXCEL</a>
+                                            </p>
+                                        </div>
+                                    </td>
+                                </tr>
+                                )}
                             </tbody>
                         </table>
 
