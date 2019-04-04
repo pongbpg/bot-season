bb9f2ff8fc81206069b8ece1c65dd44191b670c2
tawatchai.w <tawatchai.w@nextcorp.co.th>
2019-04-04 16:21:42 +0700

    kerry alpha

--- server/server.js
+++ server/server.js
@@ -704,7 +704,7 @@ const push = (obj, LINE_HEADER) => {
     });
 };
 const initMsgOrder = (txt) => {
-    const express = ["K", 'F'];
+    const express = ["K", 'A'];
     // const pages = ["@DB", "@SCR01", "@TCT01", "@TD01", "@TD02", "@TS01", "@TS02", "@TS03", "@TST", "DB", "SCR01", "SSN01", "TCT01", "TD01", "TD02", "TS01", "TS02", "TS03", "TST", "TPF01"];
     return db.collection('pages')
         .where('country', '==', 'TH')
