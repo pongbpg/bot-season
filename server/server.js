e0e9a24ef61bf7fcd8bc588fb731cdad29f088d2
tawatchai.w <tawatchai.w@nextcorp.co.th>
2019-05-08 15:28:34 +0700

    check price NaN

--- server/server.js
+++ server/server.js
@@ -1046,7 +1046,7 @@ const initMsgOrder = (txt) => {
                         checkBank = true;
                     }
                 }
-                return checkBank
+                return checkBank && !isNaN(bank.price)
                     ? bank.name + ' ' + (bank.time == '00.00' ? '' : (bank.date.indexOf('undefined') > -1 ? bank.date : moment(bank.date, 'YYYYMMDD').format('DD/MM/YY'))) + ' ' + (bank.time == '00.00' ? '' : bank.time + 'น.') + '=' + formatMoney(bank.price, 0)
                     : `${emoji(0x1000A6) + bank.name}undefined`
                 // return bank.name.indexOf('COD') > -1 && ['A', 'K', 'C'].indexOf(data.name.substr(0, 1)) == -1
