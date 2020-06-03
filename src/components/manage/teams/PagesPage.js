3f5d3c9d9cd945f21467c4cf12974b128057b91b
tawatchai.w <pong.tw34@gmail.com>
2020-06-03 22:43:53 +0700

    edit page token

--- src/components/manage/teams/PagesPage.js
+++ src/components/manage/teams/PagesPage.js
@@ -36,6 +36,7 @@ export class PagesPage extends React.Component {
 
     onNewTeamChange = (e) => {
         const country = this.state.teams.find(team => team.id == e.target.value).country
+        // console.log(this.state.newPage)
         this.setState({ newPage: { ...this.state.newPage, team: e.target.value, country } })
     }
     onNewAdminChange = (e) => {
@@ -58,11 +59,9 @@ export class PagesPage extends React.Component {
             } else {
                 if (confirm('คุณแน่ใจที่จะเพิ่มเพจนี้')) {
                     this.props.startPushPage(this.state.newPage)
-                    // .then(() => {
-                    //     this.props.startListPages(this.state.auth);
-                    // })
+                    this.props.startListPages(this.state.auth);
                 }
-                this.setState({ newPage: { ...this.state.newPage, id: '' } })
+                this.setState({ newPage: { id: '' } })
             }
         } else {
             console.log('กรุณาเลือกข้อมูลให้ครบ')
@@ -78,14 +77,25 @@ export class PagesPage extends React.Component {
         this.setState({ page: { ...this.state.page, admin: admin.name, adminId: admin.userId } })
     }
     onComChange = (e) => {
-        this.setState({ page: { ...this.state.page, ...this.state.coms.find(com => com.comId == e.target.value) } })
+        const com = this.state.coms.find(com => com.comId == e.target.value);
+        this.setState({
+            page: { ...this.state.page, ...com },
+            // pages: this.state.pages.map(m => m.id == this.state.page.id ? { ...m, ...com } : m)
+        })
     }
     onActIdChange = (e) => {
         const actId = e.target.value.replace(/\s/g, '');
-        this.setState({ page: { ...this.state.page, actId } })
+        this.setState({
+            page: { ...this.state.page, actId },
+            // pages: this.state.pages.map(m => m.id == this.state.page.id ? { ...m, actId } : m)
+        });
     }
     onActiveChange = (e) => {
-        this.setState({ page: { ...this.state.page, active: e.target.value == 'true' ? true : false } })
+        const active = e.target.value == 'true' ? true : false;
+        this.setState({
+            page: { ...this.state.page, active },
+            // pages: this.state.pages.map(m => m.id == this.state.page.id ? { ...m, active } : m)
+        })
     }
     onEditClick = (e) => {
         this.setState({ page: this.state.pages.find(f => f.id == e.target.value) })
@@ -98,10 +108,13 @@ export class PagesPage extends React.Component {
         if (this.checkObj(this.state.page)) {
             if (confirm('คุณแน่ใจที่จะแก้ไขข้อมูลเพจนี้')) {
                 this.props.startUpdatePage(this.state.page)
-                this.props.startListPages(this.state.auth);
+                // this.props.startListPages(this.state.auth);
             }
 
-            this.setState({ page: { id: '', actId: '' } })
+            this.setState({
+                pages: this.state.pages.map(m => m.id == this.state.page.id ? { ...this.state.page } : m),
+                page: { id: '', actId: '' }
+            })
         } else {
             console.log(this.state.page)
             alert('กรุณาเลือกข้อมูลให้ครบ')
@@ -178,7 +191,7 @@ export class PagesPage extends React.Component {
                                 <th className="has-text-left">Page</th>
                                 <th className="has-text-left">Admin</th>
                                 {/* <th className="has-text-left">Com</th> */}
-                                <th className="has-text-left" >Act ID</th>
+                                <th className="has-text-left">Token</th>
                                 <th className="has-text-left" >Status</th>
                                 <th className="has-text-centered" width="10%">จัดการ</th>
                             </tr>
@@ -187,7 +200,6 @@ export class PagesPage extends React.Component {
                             {this.state.pages.length > 0 ?
                                 this.state.pages.filter(f => f.team == this.state.newPage.team)
                                     .map((page, index) => {
-                                        // console.log('map', page.id, this.state.newPage.team)
                                         return this.state.page.id != page.id ? (
                                             <tr key={page.id}>
                                                 <td className='has-text-centered'>{index + 1}</td>
@@ -195,7 +207,7 @@ export class PagesPage extends React.Component {
                                                 <td>{page.id}</td>
                                                 <td>{page.admin}</td>
                                                 {/* <td>{page.comId}</td> */}
-                                                <td>{page.actId}</td>
+                                                <td>{page.actId ? 'Connected' : ''}</td>
                                                 <td>{page.active ? 'เปิด' : 'ปิด'}</td>
                                                 <td className="has-text-centered">
                                                     <button className="button"
